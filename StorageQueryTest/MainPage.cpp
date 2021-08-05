83433b083c15d9db648e11c120e647477938a522
Manohar Gunturu <mgunturu@microsoft.com>
2021-08-05 12:28:33 -0700

    first commit

---
+++ StorageQueryTest/MainPage.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+using namespace winrt::Windows::ApplicationModel::Activation;
+
+constexpr uint32_t BulkLoadStorageFileCount{ 50 };
+
+namespace winrt::StorageQueryTest::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+    }
+
+}
---
+++ StorageQueryTest/MainPage.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+using namespace winrt;
+using namespace winrt::Windows;
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::UI::Xaml;
+
+namespace WS = winrt::Windows::Storage;
+
+namespace winrt::StorageQueryTest::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+
+    };
+
+}
+
+namespace winrt::StorageQueryTest::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
